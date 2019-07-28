0e451414840bd77269c1b25ecc3dae67cff06898
Maxime ROBERT <maxime.robert1992@gmail.com>
2019-07-28 17:41:26 +0100

    chore: generate enigma app

---
+++ apps/enigma/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'enigma',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/enigma',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
