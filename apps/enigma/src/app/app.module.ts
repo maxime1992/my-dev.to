be67d5dd60780926e189647a50e5c385c8ca7bd3
Maxime ROBERT <maxime.robert1992@gmail.com>
2019-09-20 06:32:31 +0100

    feat(enigma): set default url to "encrypt"

--- apps/enigma/src/app/app.module.ts
+++ apps/enigma/src/app/app.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'decrypt',
     loadChildren: () => import('./decrypt/decrypt.module').then(m => m.DecryptModule),
   },
+  {
+    path: '**',
+    redirectTo: 'encrypt',
+  },
 ];
 
 @NgModule({
