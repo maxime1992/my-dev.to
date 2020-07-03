de72a0e47db9caee959d1870fc6b8ffc6e838cfa
Maxime Robert <maxime.robert1992@gmail.com>
2020-07-03 11:34:22 +0200

    chore(time-s-up): setup ngrx

--- apps/time-s-up-app/src/app/app.module.ts
+++ apps/time-s-up-app/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {
+      initialNavigation: 'enabled',
+    }),
+    StoreModule.forRoot({
+      router: routerReducer,
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 50,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
